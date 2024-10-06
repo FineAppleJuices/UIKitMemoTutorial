40b2aa4a7d9d81b4bacb1753ed5574a64bb34513
fine <92whdtjs@gmail.com>
2024-10-06 21:46:53 +0900

    [4주차] UICollectionView 활용하기

--- UIKitMemoTutorial/UIKitMemoTutorial/Controller/MainTabBarController.swift
+++ UIKitMemoTutorial/UIKitMemoTutorial/Controller/MainTabBarController.swift
@@ -10,14 +10,17 @@ import UIKit
 class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        let memoService = MemoService()
         
-        let memoListNavController = UINavigationController(rootViewController: MemoListViewController(memoService: MemoService()))
+        let memoListNavController = UINavigationController(rootViewController: MemoListViewController(memoService: memoService))
         memoListNavController.tabBarItem = UITabBarItem(title: "메모", image: UIImage(systemName: "note.text"), tag: 0)
+        let memoCollectionController = UINavigationController(rootViewController: MemoCollectionViewController(memoService: memoService))
+        memoCollectionController.tabBarItem = UITabBarItem(title: "사진", image: UIImage(systemName: "photo"), tag: 1)
         let settingsNavController = UINavigationController(rootViewController: SettingsViewController(style: .grouped))
-        settingsNavController.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "gear"), tag: 1)
+        settingsNavController.tabBarItem = UITabBarItem(title: "설정", image: UIImage(systemName: "gear"), tag: 2)
                
         
-        viewControllers = [memoListNavController, settingsNavController]
+        viewControllers = [memoListNavController, memoCollectionController, settingsNavController]
     }
 }
 
